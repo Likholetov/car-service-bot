8a7e49b8930fd7ea26a793b17640308b1c656e30
Alexandr Likholetov <likholetovalex@gmail.com>
2019-02-27 01:11:00 +0300

    test data

---
+++ test_data/test.js
@@ -0,0 +1,21 @@
+// Основные зависимости
+import config from 'config'
+import mongoose from 'mongoose'
+
+import database from "./database.json"
+import '../models/service.model'
+import '../models/part.model'
+
+const Service = mongoose.model('services')
+const Part = mongoose.model('parts')
+
+// подключение MongoDB
+mongoose.connect(config.get('db_url'), {
+    useNewUrlParser: true
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(err))
+
+// filling DB with test data
+database.services.forEach(s => new Service(s).save().catch(e => console.log(e)))
+database.parts.forEach(p => new Part(p).save().catch(e => console.log(e)))
